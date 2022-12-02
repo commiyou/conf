e688b51c1f92e582462d7086f38ebe2f37982bd9
commiyou <commiyou@gmail.com>
2022-12-02 14:20:13 +0800

    update zinit plugins

--- zsh/zshrc.d/misc.zsh
+++ zsh/zshrc.d/misc.zsh
@@ -1,5 +1,6 @@
 autoload -Uz allopt zed zmv zcalc colors
 colors
+alias C="zcalc -f -e"
 
 [ -f $XDG_CACHE_HOME/lvim/lvim.shada ] && export viminfo=$XDG_CACHE_HOME/lvim/lvim.shada
 [ -f $XDG_CACHE_HOME/nvim/lvim.shada ] && [ -z "$viminfo" ] && export viminfo=$XDG_CACHE_HOME/nvim/lvim.shada

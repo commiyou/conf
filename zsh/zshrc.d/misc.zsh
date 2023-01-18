32aaaa70219e5a983c0987c15387cf23bf482d27
commiyou <commiyou@gmail.com>
2023-01-18 09:51:38 +0800

    update cheat

--- zsh/zshrc.d/misc.zsh
+++ zsh/zshrc.d/misc.zsh
@@ -2,5 +2,5 @@ autoload -Uz allopt zed zmv zcalc colors
 colors
 alias C="zcalc -f -e"
 
-[ -f $XDG_CACHE_HOME/lvim/lvim.shada ] && export viminfo=$XDG_CACHE_HOME/lvim/lvim.shada
+[ -f $XDG_CACHE_HOME/lvim/lvim.shada ] && command -v lvim >/dev/null 2>&1 && export viminfo=$XDG_CACHE_HOME/lvim/lvim.shada
 [ -f $XDG_CACHE_HOME/nvim/lvim.shada ] && [ -z "$viminfo" ] && export viminfo=$XDG_CACHE_HOME/nvim/lvim.shada

a08c3701989d82920717ba99816b8177f8a8db44
Commi You <commiyou@gmail.com>
2023-11-08 15:46:27 +0800

    update nvim-osc52 & tmux config

--- lvim/config.lua
+++ lvim/config.lua
@@ -315,10 +315,20 @@ lvim.plugins = {
   { "mildred/vim-bufmru" },
   { "morhetz/gruvbox" },
   { 'ojroques/nvim-osc52',
+    -- iterm2 
     config = function()
-      vim.keymap.set('n', '<leader>c', require('osc52').copy_operator, {expr = true})
-      vim.keymap.set('n', '<leader>cc', '<leader>c_', {remap = true})
-      vim.keymap.set('v', '<leader>c', require('osc52').copy_visual)
+      require('osc52').setup {
+        max_length = 0,           -- Maximum length of selection (0 for no limit)
+        silent = false,           -- Disable message on successful copy
+        trim = true,             -- Trim surrounding whitespaces before copy
+        tmux_passthrough = true, -- Use tmux passthrough (requires tmux: set -g allow-passthrough on)
+      }
+      function copy()
+        if vim.v.event.operator == 'y' and (vim.v.event.regname == '' or vim.v.event.regname == '+') then
+          require('osc52').copy_register('+')
+        end
+      end
+      vim.api.nvim_create_autocmd('TextYankPost', {callback = copy})
     end
   },
   {
@@ -450,7 +460,3 @@ set tags=./tags;,tags;
 nnoremap gt :ts <C-R>=expand("<cword>")<CR><CR>
 
 ]])
-
-vim.keymap.set('n', '<leader>y', require('osc52').copy_operator, {expr = true})
-vim.keymap.set('n', '<leader>yy', '<leader>y_', {remap = true})
-vim.keymap.set('v', '<leader>y', require('osc52').copy_visual)

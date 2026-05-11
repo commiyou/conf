872478f9756d47b9275c313589dc297c3172821d
commiyou <commiyou@gmail.com>
2026-05-11 14:17:04 +0800

    refactor(zsh): streamline zshrc and zinit plugin management
    • Remove redundant sourcing of .self.zshrc and extra .p10k.zsh lines in .zshrc.
    • Consistently quote variables in aliases.zsh and clean up whitespace.
    • Update less alias to use LANG=C.utf8 for better encoding support.
    • Switch pprint | less alias to use fx for JSON viewing.
    • Comment out or remove rarely used global aliases (CNO, T1, WL).
    • Refactor zinit.zsh for improved readability and maintainability:
    • Organize plugin loading into clear sections (theme, OMZ, binaries, UX, completions).
    • Use zt function for parallel light-mode loading.
    • Defer heavy completions and binary plugin loading for faster shell startup.
    • Add explicit plugin configuration for forgit, zoxide, and others.
    • Remove duplicate or obsolete plugin loads.
    • Ensure proper order for fzf-tab, syntax-highlighting, and autosuggestions.
    • General code cleanup, improved comments, and consistent formatting.

--- zsh/zshrc.d/aliases.zsh
+++ zsh/zshrc.d/aliases.zsh
@@ -1,30 +1,29 @@
- [ -f $XDG_CONFIG_HOME/rc.d/aliasrc ] && source $XDG_CONFIG_HOME/rc.d/aliasrc
-
+[ -f "$XDG_CONFIG_HOME"/rc.d/aliasrc ] && source "$XDG_CONFIG_HOME"/rc.d/aliasrc
 
 alias zmv='noglob zmv -w'
 # paste online
 alias -g SPRNG=" | curl -F 'sprunge=<-' http://sprunge.us"
 
-hash -d conf=$XDG_CONFIG_HOME
-hash -d vim=$XDG_CONFIG_HOME/vim
-hash -d tmux=$XDG_CONFIG_HOME/tmux
-hash -d zsh=$XDG_CONFIG_HOME/zsh
-hash -d rc=$XDG_CONFIG_HOME/rc.d
-hash -d cache=$XDG_CACHE_HOME
-hash -d data=$XDG_DATA_HOME
-hash -d lvim=$XDG_DATA_HOME/lunarvim/
-hash -d cheat=$XDG_DATA_HOME/cheat/cheatsheets/personal
+hash -d conf="$XDG_CONFIG_HOME"
+hash -d vim="$XDG_CONFIG_HOME"/vim
+hash -d tmux="$XDG_CONFIG_HOME"/tmux
+hash -d zsh="$XDG_CONFIG_HOME"/zsh
+hash -d rc="$XDG_CONFIG_HOME"/rc.d
+hash -d cache="$XDG_CACHE_HOME"
+hash -d data="$XDG_DATA_HOME"
+hash -d lvim="$XDG_DATA_HOME"/lunarvim/
+hash -d cheat="$XDG_DATA_HOME"/cheat/cheatsheets/personal
 
 # vim last modified file in current dir
-alias -g NF='*(.om[1])'  # newest file
-alias -g ND='*(/om[1])'  # newest directory
+alias -g NF='*(.om[1])' # newest file
+alias -g ND='*(/om[1])' # newest directory
 alias vn='vim *(.om[1]^D)'
 
-alias -g L=" | less"
-alias -g LC="| command less -R"  # less with colors support
+alias -g L=" | LANG=C.utf8 less"
+alias -g LC="| command less -R" # less with colors support
 alias -g EG=" | LC_ALL=C command egrep -i --color=auto -a"
 alias -g G=" | LC_ALL=C command grep -i --color=auto -a"
-alias -g P=" | pprint | less"
+alias -g P=" | fx"
 alias -g NE="2> /dev/null"
 alias -g NUL="> /dev/null 2>&1"
 alias -g GBK=" | iconv -f utf8 -t gb18030 -c"
@@ -38,18 +37,18 @@ _column_no() {
   sep="${1:-\t}"
   head -1 | xargs -n1 -d"$sep" | cat -n
 }
-alias -g CNO=" | _column_no"
+#alias -g CNO=" | _column_no"
 alias -g H=" | head"
-alias -g H1=" | head -1"
+#alias -g H1=" | head -1"
 alias -g T=" | tail "
-alias -g T1=" | tail -1"
-alias -g WL=" | wc -l"
+#alias -g T1=" | tail -1"
+#alias -g WL=" | wc -l"
 alias -g F="| fzf --tmux"
 
-alias -g E="luit -encoding gb18030"  # env gb18030 to utf
-if type wslview > /dev/null; then
+alias -g E="luit -encoding gb18030" # env gb18030 to utf
+if type wslview >/dev/null; then
   alias open="wslview"
-elif type xdg-open > /dev/null; then
+elif type xdg-open >/dev/null; then
   alias open=xdg-open
 fi
 
@@ -57,5 +56,5 @@ fi
 # global string replacement you can use string=replace syntax
 # echo foo bar baz foo
 # fc -e - foo=boo     # echo boo bar baz boo
-alias r='fc -e -'  
+alias r='fc -e -'
 alias vimdiff="vim -d"
--- zsh/zshrc.d/zinit.zsh
+++ zsh/zshrc.d/zinit.zsh
@@ -1,8 +1,7 @@
 # vim: filetype=zsh
-
-SHELL=zsh
-#command -v ruby > /dev/null && command -v gem >/dev/null || return
 [ -n "$USE_ZPLUG" ] && return
+
+# ─── §1 Bootstrap ─────────────────────────────────────────────────────────────
 
 typeset -A ZINIT=(
   BIN_DIR         ${XDG_DATA_HOME:-$HOME/.local/share}/zinit/bin
@@ -10,59 +9,69 @@ typeset -A ZINIT=(
   ZCOMPDUMP_PATH  ${XDG_CACHE_HOME:-$HOME/.cache}/zcompdump-${(%):-%n}
   COMPINIT_OPTS   -C
 )
-
 hash -d zinit=$ZINIT[HOME_DIR]
 
-### Added by Zinit's installer
 if [[ ! -f $ZINIT[BIN_DIR]/zinit.zsh ]]; then
-  print -P "%F{33}▓▒░ %F{220}Installing %F{33}DHARMA%F{220} \
-    Initiative Plugin Manager (%F{33}zdharma/zinit%F{220})…%f"
+  print -P "%F{33}▓▒░ %F{220}Installing zinit…%f"
   command mkdir -p "$ZINIT[HOME_DIR]" && command chmod g-rwX "$ZINIT[HOME_DIR]"
   command git clone https://github.com/zdharma-continuum/zinit "$ZINIT[BIN_DIR]" && \
-    print -P "%F{33}▓▒░ %F{34}Installation successful.%f%b" || \
-    print -P "%F{160}▓▒░ The clone has failed.%f%b"
+    print -P "%F{34}▓▒░ Installation successful.%f" || \
+    print -P "%F{160}▓▒░ Clone failed.%f"
 fi
 
 source $ZINIT[BIN_DIR]/zinit.zsh
 
+# zt: shorthand for deferred parallel light loads; pass 'wait' to defer
 zt(){ zinit depth'3' light-mode lucid "${@}"; }
-#### plugins
-## light is parallel & load is sequential.
-#load=load
-#[[ $load == light ]] && lightmode=light-mode || lightmode=
 
-
-zt for \
-  romkatv/powerlevel10k \
-  atload"source ${ZDOTDIR:-$HOME}/.p10k.zsh" \
-  zdharma-continuum/null
-
-
+# Annexes must be first — they extend zinit's ice syntax for everything below
 zt for \
   zdharma-continuum/z-a-patch-dl \
   zdharma-continuum/z-a-submods \
   NICHOLAS85/z-a-linkman \
   NICHOLAS85/z-a-linkbin \
-  atinit'Z_A_USECOMP=1' \
-  NICHOLAS85/z-a-eval
+  atinit'Z_A_USECOMP=1' NICHOLAS85/z-a-eval
 
-# use zinit recache to reeval
+
+# ─── §2 Theme ─────────────────────────────────────────────────────────────────
+# Synchronous: p10k instant-prompt demands the theme load before first prompt
+
+zt for \
+  atload"[[ -f ${ZDOTDIR:-$HOME}/.p10k.zsh ]] && source ${ZDOTDIR:-$HOME}/.p10k.zsh" \
+  romkatv/powerlevel10k
+
+
+# ─── §3 Immediate plugins ─────────────────────────────────────────────────────
+# zsh-vi-mode: ZVM config vars must be exported before zinit sources the plugin
+
+ZVM_LINE_INIT_MODE=$ZVM_MODE_INSERT
+zinit depth'1' light-mode lucid for jeffreytse/zsh-vi-mode
+
+
+# ─── §4 Deferred: environment ─────────────────────────────────────────────────
+# zicompinit runs first so subsequent atload hooks can call compdef
+
 zt wait for \
+  atinit'zicompinit' zdharma-continuum/null \
   eval'dircolors -b LS_COLORS' \
   atload'zstyle ":completion:*" list-colors ${(s.:.)LS_COLORS}' \
-  commiyou/LS_COLORS
-
-zt wait for \
-  trigger-load'!zhooks' \
-  agkozak/zhooks \
+  commiyou/LS_COLORS \
+  trigger-load'!zhooks' agkozak/zhooks \
   trigger-load'!gencomp' blockf \
-  atload"!export GENCOMP_DIR=${ZDOTDIR:-$HOME}/completions/" Aloxaf/gencomp \
-  blockf compile'lib/*f*~*.zwc' \
-  pick'autoenv.zsh' nocompletions \
-  Tarrasch/zsh-autoenv 
+    atload'export GENCOMP_DIR=${ZDOTDIR:-$HOME}/completions/' Aloxaf/gencomp \
+  blockf compile'lib/*f*~*.zwc' pick'autoenv.zsh' nocompletions \
+    Tarrasch/zsh-autoenv
 
-zinit ice wait="0" lucid from="gh-r" as="program" pick="zoxide-*/zoxide -> zoxide" cp="zoxide-*/completions/_zoxide -> _zoxide" atclone="./zoxide init zsh > init.zsh" atpull="%atclone" src="init.zsh"
-zinit light ajeetdsouza/zoxide
+# zoxide: uses custom pick/cp/atclone so written out explicitly
+zinit wait lucid light-mode for \
+  from'gh-r' as'program' \
+  pick'zoxide-*/zoxide -> zoxide' \
+  cp'zoxide-*/completions/_zoxide -> _zoxide' \
+  atclone'./zoxide init zsh > init.zsh' atpull'%atclone' src'init.zsh' \
+  ajeetdsouza/zoxide
+
+
+# ─── §5 Deferred: OMZ plugins ─────────────────────────────────────────────────
 
 zt wait for \
   OMZP::fancy-ctrl-z \
@@ -73,176 +82,107 @@ zt wait for \
   OMZP::rsync \
   OMZP::systemadmin \
   OMZP::urltools \
-  trigger-load'!pp_json;!is_json;!urlencode_json;!urldecode_json;!pp_ndjson;!is_ndjson;' \
-  OMZP::jsontools \
-  trigger-load'!x;!extract;' \
-  OMZP::extract \
-  trigger-load"!fuck" OMZP::thefuck \
+  trigger-load'!pp_json;!is_json;!urlencode_json;!urldecode_json;!pp_ndjson;!is_ndjson' \
+    OMZP::jsontools \
+  trigger-load'!x;!extract' OMZP::extract \
+  trigger-load'!fuck' OMZP::thefuck \
   OMZL::clipboard.zsh \
   OMZL::completion.zsh \
   OMZL::functions.zsh \
   OMZL::grep.zsh \
-  has'tmux' atload$'!ZSH_TMUX_FIXTERM=false; \
+  has'tmux' atload'ZSH_TMUX_FIXTERM=false; \
     ZSH_TMUX_CONFIG=${XDG_CONFIG_HOME:-$HOME/.config}/tmux/tmux.conf; \
-    compdef _zsh_tmux_plugin_run=tmux; ' \
-  OMZP::tmux 
-
-#if'[[ -z "$commands[tig]" ]]' @jonas/tig \
-zt wait binary from"gh-r" lman lbin for \
-  if'[[ -z "$commands[exa]" ]]' @ogham/exa \
-  if'[[ -z "$commands[fd]" ]]' @sharkdp/fd  \
-  if'[[ -z "$commands[gron]" ]]' @tomnomnom/gron  \
-  if'[[ -z "$commands[jless]" ]]' PaulJuliusMartinez/jless \
-  if'[[ -z "$commands[bat]" ]]' @sharkdp/bat  \
-  if'[[ -z "$commands[mdcat]" ]]' @swsnr/mdcat \
-  if'[[ -z "$commands[xsv]" ]]' BurntSushi/xsv \
-  if'[[ -z "$commands[sad]" ]]' ms-jpq/sad \
-  id-as'fx-bin' lbin"fx* -> fx"  \
-  if'[[ -z "$commands[fx]" ]]' antonmedv/fx  \
-  id-as'tmux-bin' lbin"tmux* -> tmux" ver"v3.3a"  \
-  mjakob-gh/build-static-tmux 
+    compdef _zsh_tmux_plugin_run=tmux' \
+    OMZP::tmux
 
 
+# ─── §6 Deferred: binary tools (gh-r) ────────────────────────────────────────
+# Install only when the command is absent; lbin/lman wire up links automatically
 
-zt wait binary from"gh-r" for \
-  lman lbin"**/rg -> rg" \
-  if'[[ -z "$commands[rg]" ]]' @BurntSushi/ripgrep \
-  dl'https://raw.githubusercontent.com/junegunn/fzf/master/man/man1/fzf.1' \
-  id-as'fzf-bin' lman lbin \
-  if'[[ -z "$commands[fzf]" ]]' junegunn/fzf \
-  id-as'cheat-bin' lman lbin"**/cheat* -> cheat" \
-  cheat/cheat
+zt wait binary from'gh-r' lman lbin for \
+  if'[[ -z $commands[exa]   ]]' @ogham/exa \
+  if'[[ -z $commands[fd]    ]]' @sharkdp/fd \
+  if'[[ -z $commands[bat]   ]]' @sharkdp/bat \
+  if'[[ -z $commands[gron]  ]]' @tomnomnom/gron \
+  if'[[ -z $commands[jless] ]]' PaulJuliusMartinez/jless \
+  if'[[ -z $commands[mdcat] ]]' @swsnr/mdcat \
+  if'[[ -z $commands[xsv]   ]]' BurntSushi/xsv \
+  if'[[ -z $commands[sad]   ]]' ms-jpq/sad \
+  id-as'fx-bin'   lbin'fx* -> fx'                if'[[ -z $commands[fx]  ]]' antonmedv/fx \
+  id-as'tmux-bin' lbin'tmux* -> tmux' ver'v3.3a' mjakob-gh/build-static-tmux
 
-zinit wait lucid light-mode for from'gh-r' as'program' id-as'navi' pick'navi' denisidoro/navi
+zt wait binary from'gh-r' lman for \
+  lbin'**/rg -> rg'   if'[[ -z $commands[rg]  ]]' @BurntSushi/ripgrep \
+  id-as'fzf-bin' lbin if'[[ -z $commands[fzf] ]]' \
+    dl'https://raw.githubusercontent.com/junegunn/fzf/master/man/man1/fzf.1' \
+    junegunn/fzf \
+  id-as'cheat-bin' lbin'**/cheat* -> cheat' cheat/cheat
 
-zt wait for reegnz/jq-zsh-plugin
+zt wait binary from'gh-r' as'program' for \
+  id-as'navi' pick'navi' denisidoro/navi
 
-# # Always starting with insert mode for each command line
-zinit ice depth=1
-zinit light jeffreytse/zsh-vi-mode
-#
-ZVM_LINE_INIT_MODE=$ZVM_MODE_INSERT
-
-
-zinit ice as"program" pick"$ZPFX/bin/git-*" src"etc/git-extras-completion.zsh" make"PREFIX=$ZPFX"
-zinit light tj/git-extras
-
-#
-#zinit ice if'[[ -n "$commands[rg]" ]]' binary from"gh-r" lman lbin"**/rg -> rg" 
-#zinit load @BurntSushi/ripgrep
-#
-#zinit ice if'[[ -n "$commands[cheat]" ]]' binary id-as'cheat-bin' lman lbin"**/cheat* -> cheat"
-#zinit load cheat/cheat
-#
-
-export AUTOSWITCH_DEFAULT_CONDAENV="base"
+# git-extras: make build; deferred fine since git-* are non-core commands
 zt wait for \
-  atinit"local zew_word_style=whitespace" \
-  zdharma-continuum/zsh-editing-workbench \
-  multisrc'shell/*.zsh' \
-  trackbinds bindmap='^T -> ^X^T; \ec -> ^X^C' \
-  junegunn/fzf  \
-  bckim92/zsh-autoswitch-conda
+  as'program' pick"$ZPFX/bin/git-*" src'etc/git-extras-completion.zsh' make"PREFIX=$ZPFX" \
+  tj/git-extras
 
-# zt wait for \
-#   atload'!export CHEAT_USE_FZF=true' pick'scripts/cheat.zsh' \
-#   cheat/cheat \
-#   id-as'cheat.sh' \
-#   binary \
-#   nocompile \
-#   dl'https://cht.sh/:cht.sh -> cht.sh' \
-#   atinit'chmod +x cht.sh' lbin'cht.sh' \
-#   zdharma-continuum/null
 
-# zt wait has'lua' for \
-#   atload'!export _ZL_DATA=$XDG_CACHE_HOME/.zlua;' \
-#   skywind3000/z.lua \
-#   atload'!function _z() { _zlua "$@"; }; alias z="nocorrect _fz"' \
-#   commiyou/fz.sh
-
-#zplugin ice wait'1' lucid
-#zplugin light laggardkernel/zsh-thefuck
-
-#as"program" pick'bin/fzf-tmux' src'shell/key-bindings.zsh'  trackbinds bindmap='^T -> ^X^T; \ec -> ^X\ec' commiyou/fzf \
-# $'string'  quote https://stackoverflow.com/questions/1250079/how-to-escape-single-quotes-within-single-quoted-strings/16605140#16605140
-#ptavares/zsh-direnv \
-
-__forgit_atload() {
-    export FORGIT_INSTALL_DIR="$PWD"
-    export FORGIT_NO_ALIASES=1
-    export FORGIT_LOG_FZF_OPTS='--bind="ctrl-e:execute(echo {} |grep -Eo [a-f0-9]+ |head -1 |xargs command git show |vim -)"'
-    alias gdca="forgit::diff --cached"
-    alias gds="forgit::diff --cached"
-    alias glog="forgit::log --oneline --decorate --graph"
-}
-#__forgit_atload
+# ─── §7 Deferred: UX plugins ──────────────────────────────────────────────────
 
 zt wait for \
   hlissner/zsh-autopair \
   Tarrasch/zsh-functional \
   Tarrasch/zsh-colors \
+  reegnz/jq-zsh-plugin \
   atload'ZSH_COMMAND_TIME_EXCLUDE=(vim v); ZSH_COMMAND_TIME_COLOR=red' \
-  popstas/zsh-command-time \
-  atpull$'git config --global core.pager "diff-so-fancy | less --tabs=4 -RFX"; \
-    git config --global interactive.diffFilter "diff-so-fancy --patch";' \
-  z-shell/zsh-diff-so-fancy \
-  atload$'!__forgit_atload;compdef _git gco=git-checkout;' wfxr/forgit \
+    popstas/zsh-command-time \
+  atpull'git config --global core.pager "diff-so-fancy | less --tabs=4 -RFX"; \
+    git config --global interactive.diffFilter "diff-so-fancy --patch"' \
+    z-shell/zsh-diff-so-fancy \
+  atload'export FORGIT_INSTALL_DIR=$PWD; \
+    export FORGIT_NO_ALIASES=1; \
+    export FORGIT_LOG_FZF_OPTS="--bind=ctrl-e:execute(echo {} |grep -Eo [a-f0-9]+ |head -1 |xargs command git show |vim -)"; \
+    alias gdca="forgit::diff --cached"; alias gds="forgit::diff --cached"; \
+    alias glog="forgit::log --oneline --decorate --graph"; \
+    compdef _git gco=git-checkout' \
+    wfxr/forgit
 
-  # as"program" atload'export SSHHOME=$XDG_CONFIG_HOME' pick'sshrc' IngoMeyer441/sshrc \
-  # atinit"local zew_word_style=whitespace" \
-  # zdharma-continuum/zsh-editing-workbench 
-
-
-#zt wait'1' for \
-#  trigger-load'!conda;!ipython;!pip;!python;' commiyou/conda-init-zsh-plugin \
-#  blockf as'completion' \
-#  conda-incubator/conda-zsh-completion
-
-
-# completions
-#zt wait blockf as"completion" for \
-#  svn OMZP::fd \
-#  svn OMZP::docker \
-#  svn OMZP::ripgrep
+zt wait for \
+  atinit'local zew_word_style=whitespace' zdharma-continuum/zsh-editing-workbench \
+  multisrc'shell/*.zsh' trackbinds bindmap'^T -> ^X^T; \ec -> ^X^C' junegunn/fzf \
+  atinit'export AUTOSWITCH_DEFAULT_CONDAENV=base' bckim92/zsh-autoswitch-conda
 
 zt wait'[[ -n $WORK_ENV ]]' id-as'work-env' for \
   $XDG_CONFIG_HOME/work/$WORK_ENV
 
-zt wait for \
-  blockf as'completion' atpull'zinit creinstall -q .' id-as'commiyou-completions'\
-  $XDG_CONFIG_HOME/zsh/completions
+
+# ─── §8 Completions ───────────────────────────────────────────────────────────
+# Ordering contract:
+#   wait §4 (zicompinit first) → wait §5–§7 → wait'0b' (zicdreplay + fzf-tab → fsh → autosuggestions)
+#   wait'1'      → zsh-more-completions (heavy; loads after first prompt)
+#
+# fzf-tab-source provides extra previews; must load before fzf-tab
+
+zinit wait lucid light-mode depth'1' if'(($+commands[fzf]))' for \
+  Freed-Wu/fzf-tab-source
 
 
-zinit ice lucid nocompile wait'0e' nocompletions
+# fzf-tab > fast-syntax-highlighting > autosuggestions: order is mandatory
+zstyle ':fzf-tab:*' prefix ''
+zstyle ':fzf-tab:*' single-group prefix color header
+
+zt wait'0b' for \
+  atinit'zicdreplay' Aloxaf/fzf-tab \
+  zdharma-continuum/fast-syntax-highlighting \
+  blockf atpull'zinit creinstall -q .' zsh-users/zsh-completions \
+  atload'_zsh_autosuggest_start' zsh-users/zsh-autosuggestions
+
+# Heavy completion database; load after first prompt to avoid blocking
+zinit ice lucid compile'**/*.zsh' wait'1' nocompletions
 zinit load MenkeTechnologies/zsh-more-completions
 
 
-# 使用 zinit 处理特定的补全脚本
-for completion in "$ZDOTDIR/completions"/_*; do
-  [ -f "$completion" ] && zinit ice as"completion"
-  [ -f "$completion" ] && zinit snippet "$completion"
-done
+# ─── Utilities ────────────────────────────────────────────────────────────────
 
-#zinit ice $lightmode wait lucid pick'roszsh' id-as'roszsh'
-#zinit snippet https://raw.githubusercontent.com/ros/ros/melodic-devel/tools/rosbash/roszsh
-
-#FZF_TMUX_HEIGHT=100%
-zstyle ':fzf-tab:*' prefix ''
-zstyle ':fzf-tab:*' single-group prefix color header
-zinit id-as depth'1' wait lucid \
-  if'(($+commands[fzf]))' \
-  for Freed-Wu/fzf-tab-source
-
-  #atload"!zstyle ':fzf-tab:complete:(cd|z|ls|vim|rm|rmr):*' \
-  #fzf-preview 'exa -1 --color=always \$realpath';" \
-# fzf-tab must before Fast-syntax-highlighting & autosuggestions
-# and bellow LS_COLORS
-zt wait'0b' for \
-  atinit"zicompinit; zicdreplay;" \
-  Aloxaf/fzf-tab \
-  zdharma-continuum/fast-syntax-highlighting \
-  blockf atpull'zinit creinstall -q .' zsh-users/zsh-completions \
-  atload"!_zsh_autosuggest_start" zsh-users/zsh-autosuggestions 
-
-zupdate() { [[ $# -eq 0 ]] && zinit update --all --parallel || zinit update --parallel 15 "$@" }
-zt wait'0c' for atload'compdef _zinit zi; compdef _zinit_update zupdate;' zdharma-continuum/null
+zupdate() { [[ $# -eq 0 ]] && zinit update --all --parallel || zinit update --parallel 15 "$@"; }
+zt wait'0c' for atload'compdef _zinit zi; compdef _zinit_update zupdate' zdharma-continuum/null
